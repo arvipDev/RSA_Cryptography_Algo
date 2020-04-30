29e495762a68bc3cd7f07ef00c8a329561ea28cf
arvipDev <arvind.purushotham@gmail.com>
2020-04-30 13:29:16 +0530

    Encrypt Decrypt UI creation - Stage 3 Event handlers added to the two buttons. Completed UI Creation as per initial design/plan

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,31 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class Controller {
 
+    @FXML
+    public TextArea input_textArea;
 
+    @FXML
+    public Label encrypt_label;
+
+    @FXML
+    public Label decrypt_label;
+
+    private String text;
+
+    @FXML
+    public void onEncryptButtonClicked () {
+        text = input_textArea.getText();
+        encrypt_label.setText(text);
+    }
+
+    @FXML
+    public void onDecryptButtonClicked () {
+        System.out.println("in decrypt " + text);
+        decrypt_label.setText(text);
+    }
 }
